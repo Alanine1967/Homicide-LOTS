46e6f5832d5ef8c42c6d0a85b469c5b96f7aa8a0
Steve Dodkin <stevedodkin@mac.com>
2012-09-20 11:39:59 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomicideSite::Application.config.secret_token = '67f5da062e83e75015ecd516cab143c7608f19efb4eb92571311464acf25b87e771296b32e618e7e50ba4d1f15c86159911f42a10013b8f4fff8e71b46b011ca'
